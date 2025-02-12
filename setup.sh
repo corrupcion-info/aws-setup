f979d5a92f6bff33eb4c5fd4fe37cf823e20b4d9
corrupcion.info <corrupcion.es@protonmail.com>
2025-02-12 16:22:17 +0100

    Update setup.sh
    Adding no interactive to the certbot

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo systemctl start docker.service
 DOCKERHUB_TOKEN=$(aws secretsmanager get-secret-value --secret-id dockerhub --query SecretString --output text)
 sudo docker login --username corrupcion --password $DOCKERHUB_TOKEN
 sudo docker pull corrupcion/corrupcion:latest
-sudo docker run --hostname certbot -p 80:80 -v /opt/certbot/certbot_logs:/var/log/letsencrypt -v /etc/letsencrypt:/etc/letsencrypt certbot/certbot certonly --standalone --email corrupcion.es@protonmail.com --agree-tos --no-eff-email -d corrupcion.info
+sudo docker run --hostname certbot -p 80:80 -v /opt/certbot/certbot_logs:/var/log/letsencrypt -v /etc/letsencrypt:/etc/letsencrypt certbot/certbot certonly --non-interactive --standalone --email corrupcion.es@protonmail.com --agree-tos --no-eff-email -d corrupcion.info
 sudo docker run -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt -ti --rm --entrypoint gunicorn corrupcion/corrupcion:latest -b 0.0.0.0:443 --certfile=/etc/letsencrypt/live/corrupcion.info/fullchain.pem --keyfile=/etc/letsencrypt/live/corrupcion.info/privkey.pem app:app
